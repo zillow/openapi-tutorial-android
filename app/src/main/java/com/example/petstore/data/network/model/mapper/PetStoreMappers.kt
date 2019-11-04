24ed299ffb47001214f2efa8dad14d41c2a6efb8
gagee-trulia <gagee@zillowgroup.com>
2019-11-04 13:08:18 -0800

    Now reusing objects, reducing the amount of translation layer logic

--- app/src/main/java/com/example/petstore/data/network/model/mapper/PetStoreMappers.kt
+++ app/src/main/java/com/example/petstore/data/network/model/mapper/PetStoreMappers.kt
@@ -2,22 +2,14 @@ package com.example.petstore.data.network.model.mapper
 
 import com.example.petstore.domain.model.PetDomainModel
 import com.example.petstore.domain.model.PetStoreDomainModel
-import com.example.petstore.pets.models.Pet
+import com.example.petstore.shared.models.Pet
 import com.example.petstore.store.models.StoreInfo
-import com.example.petstore.store.models.Pet as StorePet
 
 fun Pet.toDomain() = PetDomainModel(
     id = id,
     name = name,
     age = age,
     type = type
-)
-
-fun StorePet.toDomain() = PetDomainModel(
-    id = petId,
-    name = petName,
-    age = petAge,
-    type = petType
 )
 
 fun StoreInfo.toDomain() = PetStoreDomainModel(
